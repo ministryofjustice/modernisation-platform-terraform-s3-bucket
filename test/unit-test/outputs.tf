ecc4261bdbc153f76c1083f3e2d88a745dd60ecc
SteveLinden <stephen.linden@digital.justice.gov.uk>
2022-06-16 14:16:05 +0100

    Additional test added

--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -2,3 +2,7 @@ output "bucketArn" {
   value       = module.s3.bucket.arn
   description = "Bucket ARN"
 }
+output "bucketname" {
+  value       = module.s3.bucket.id
+  description = "Bucket Name"
+}
