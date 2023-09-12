fb3bb4d3807ff308d977bc9bfb3038cabafc4767
SteveLinden <stephen.linden@digital.justice.gov.uk>
2023-09-12 16:34:05 +0100

    Replacing fix/checkov-errors

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -1,4 +1,6 @@
 module "s3" {
+# This is not needed in our tests
+  #checkov:skip=CKV_AWS_300: "Ensure S3 lifecycle configuration sets period for aborting failed uploads"
   source = "../.."
   providers = {
     aws.bucket-replication = aws
