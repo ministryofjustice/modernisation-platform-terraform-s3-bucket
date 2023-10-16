4a8229209ace4a29be5b8497abbdd81b3babfa37
SteveLinden <stephen.linden@digital.justice.gov.uk>
2023-10-16 13:31:55 +0100

    #checkov:skip=CKV_AWS_26: it's part of the testing routine so doesn't require the check

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -35,8 +35,7 @@ data "aws_iam_policy_document" "topic" {
   }
 }
 resource "aws_sns_topic" "topic" {
-  # Encryption not required as topic only available during test run
-  #checkov:skip=CKV_AWS_26: "Ensure all data stored in the SNS topic is encrypted"
+  #checkov:skip=CKV_AWS_26: "Encryption not required as topic only available during test run"
   name   = "s3-event-notification-topic"
   policy = data.aws_iam_policy_document.topic.json
 }
