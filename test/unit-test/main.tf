927b6f48a37c27670b4bd18c8484d04855992fc7
matt-heery <matt.heery@digital.justice.gov.uk>
2024-09-20 13:30:13 +0100

    get vars right

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -116,7 +116,11 @@ module "s3_with_log_bucket" {
   }
   bucket_prefix = "unit-test-bucket-with-logs"
   force_destroy = true
-  log_buckets   = tomap({ "log_bucket" : module.dummy_s3_log_bucket.bucket, "log_bucket_policy" : module.dummy_s3_log_bucket.bucket_policy })
+  log_buckets   = tomap({
+    "log_bucket_name" : module.dummy_s3_log_bucket.bucket.id,
+    "log_bucket_arn" : module.dummy_s3_log_bucket.bucket.arn,
+    "log_bucket_policy" : module.dummy_s3_log_bucket.bucket_policy.policy,
+    })
   log_prefix    = "logs/"
   tags          = local.tags
 }
