e9c85e4979c24868d91045003556f55121c75bf7
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-10-16 11:15:36 +0100

    distinct-ise the policies

--- main.tf
+++ main.tf
@@ -252,17 +252,19 @@ locals {
       )
     ),
     {
-      Statement = concat(
-        jsondecode(
-          coalesce(
-            var.log_buckets["log_bucket_policy"],
-            jsonencode({
-              Version   = "2012-10-17",
-              Statement = []
-            })
-          )
-        ).Statement,
-        [local.new_policy_statements]
+      Statement = distinct(
+        concat(
+          jsondecode(
+            coalesce(
+              var.log_buckets["log_bucket_policy"],
+              jsonencode({
+                Version   = "2012-10-17",
+                Statement = []
+              })
+            )
+          ).Statement,
+          [local.new_policy_statements]
+        )
       )
     }
   ) : null
