fe6544adb4c391aba8415924d173fbc52ea40463
Aaron Robinson <41325732+ASTRobinson@users.noreply.github.com>
2025-07-11 17:07:00 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 6.3"
+      version               = "~> 6.0"
       configuration_aliases = [aws.bucket-replication]
     }
   }
-  required_version = ">= 1.0.1"
+  required_version = "~> 1.0"
 }
 
 # The default calling provider is inherited here, so we only need to create
